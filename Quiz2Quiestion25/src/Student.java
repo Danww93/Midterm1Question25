bd6e5a56d6d35a889347517b7f9516e525607122
danww93 <danww@udel.edu>
2014-09-29 20:38:18 -0400

    Midterm1Question25

---
+++ Quiz2Quiestion25/src/Student.java
@@ -0,0 +1,18 @@
+
+public class Student extends Person
+{
+	private final String classStatus;
+	
+	public Student(String classStatus)
+	{
+		this.classStatus = classStatus;
+	}
+		 
+	public String get()
+	{
+		 return classStatus;
+	}
+		 
+
+
+}
